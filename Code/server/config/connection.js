a7c06fd76fa6844f6c0cbe1144c2c5e7d95999bf
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-07-25 16:01:44 -0400

    importing base files and seeing the changes needed

---
+++ Code/server/config/connection.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://josiezobel:ssYrp4KFjNw3LqfZ@cluster0.7bvbkhn.mongodb.net/');
+
+module.exports = mongoose.connection;
